5142bb22655e38bdf9adc0f875eaa4fe9d7cc7ce
cokin <cokincahya@gmail.com>
2020-06-19 02:49:16 +0800

    Update edit.blade.php

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -6,6 +6,7 @@
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
   <meta content="" name="keywords">
   <meta content="" name="description">
+  <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> 
 
   <!-- Favicons -->
   <link href="{{asset('css/img/favicon.png')}}" rel="icon">
