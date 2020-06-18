706d9b2f10c99b1b2c6e81b36b662852c012e2f8
cokin <cokincahya@gmail.com>
2020-06-19 07:35:23 +0800

    Update create.blade.php

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -4,11 +4,13 @@
   <title>COVID Bali</title>
   <meta charset="utf-8">
    
-  <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> 
+  
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
   <meta content="" name="keywords">
   <meta content="" name="description">
-    
+   <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">  
+   
+   
    <link href="{{asset('css/img/favicon.png')}}" rel="icon">
   <link href="{{asset('css/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
 <!-- Bootstrap CSS File -->
